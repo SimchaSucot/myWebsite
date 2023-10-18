2ecc216a6b8ffc071f3f7c29858e89c75d7273fb
Simcha Sucot <simchasucot@gmail.com>
2023-10-18 15:14:33 +0300

    Added a link to the site's project

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -7,7 +7,8 @@ function Projects() {
       <div className="allProjects">
         <div className="project">
           <h3>This site is my project</h3>
-          Later I will upload a more detailed explanation here
+          Later I will upload a more detailed explanation here <br />
+          to read the code: <a href="https://github.com/SimchaSucot/myWebsite">Click here</a>
         </div>
         <div className="project">
           Soon I will upload here the plans and projects with a detailed
