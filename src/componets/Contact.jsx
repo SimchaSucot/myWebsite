924356c84c7510553b60c8977c4ee1aafe53a821
Simcha Sucot <simchasucot@gmail.com>
2023-10-19 18:19:09 +0300

    Update: The link will open in a new tab

--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -3,7 +3,7 @@ import imgWhatsApp from "/whatsApp.png";
 import imgLinkedin from "/linkedin-icon.png";
 import imgEmail from "/email_icon.png";
 import imgGithub from "/github_icon.png";
-import imgPhone from "/phone_icon.jpg"
+import imgPhone from "/phone_icon.jpg";
 
 function Contact() {
   return (
@@ -11,16 +11,16 @@ function Contact() {
       <div className="containerContact">
         <h1>⇓ Contact me ⇓</h1>
         <div>
-          <a href="https://api.whatsapp.com/send?phone=9720508726544&text=%D7%94%D7%99%D7%99">
+          <a href="https://api.whatsapp.com/send?phone=9720508726544&text=%D7%94%D7%99%D7%99" target="_blank">
             <img className="img" src={imgWhatsApp} alt="WhatsApp" />
           </a>
-          <a href="https://www.linkedin.com/in/simcha-sucot-935a9b263/">
+          <a href="https://www.linkedin.com/in/simcha-sucot-935a9b263/" target="_blank">
             <img className="img" src={imgLinkedin} alt="Linkedin" />
           </a>
-          <a href="mailto:simchasucot@gmail.com">
+          <a href="mailto:simchasucot@gmail.com" target="_blank">
             <img className="img" src={imgEmail} alt="Gmail" />
           </a>
-          <a href="https://github.com/SimchaSucot">
+          <a href="https://github.com/SimchaSucot" target="_blank">
             <img className="img" src={imgGithub} alt="Github" />
           </a>
           <a href="tel:+972508726544">
@@ -31,7 +31,7 @@ function Contact() {
       <div>
         <a
           id="fixedButton"
-          href="https://api.whatsapp.com/send?phone=9720508726544&text=%D7%94%D7%99%D7%99"
+          href="https://api.whatsapp.com/send?phone=9720508726544&text=%D7%94%D7%99%D7%99" target="_blank"
         >
           <img className="img" src={imgWhatsApp} alt="WhatsApp" />
         </a>
@@ -40,4 +40,4 @@ function Contact() {
   );
 }
 
-export default Contact;
+export default Contact
--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -8,12 +8,12 @@ function Projects() {
         <div className="project">
           <h3>This site is my project</h3>
           Later I will upload a more detailed explanation here <br />
-          to read the code: <a href="https://github.com/SimchaSucot/myWebsite">Click here</a>
+          to read the code: <a href="https://github.com/SimchaSucot/myWebsite" target="_blank">Click here</a>
         </div>
         <div className="project">
         <h3>memory Game in Python</h3>
         I will post a more detailed explanation about the game later<br />
-        to read the code: <a href="https://github.com/SimchaSucot/memoryGame">Click here</a>
+        to read the code: <a href="https://github.com/SimchaSucot/memoryGame" target="_blank">Click here</a>
         </div>
         <div className="project">
           Soon I will upload here the plans and projects with a detailed
