8a560b2d110e28d3765e755eeeb829e178857fb3
Simcha Sucot <simchasucot@gmail.com>
2023-10-20 03:25:17 +0300

    updating

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -2,18 +2,27 @@ import "./Projects.css";
 
 function Projects() {
   return (
-    <div className="containerProjects">
+    
+    <>
       <h1>My projects:</h1>
       <div className="allProjects">
         <div className="project">
           <h3>This site is my project</h3>
-          Later I will upload a more detailed explanation here <br />
-          to read the code: <a href="https://github.com/SimchaSucot/myWebsite" target="_blank">Click here</a>
+          Later I will upload a more detailed explanation here 
+          <br /><br />
+          to read the code:
+          <a href="https://github.com/SimchaSucot/myWebsite" target="_blank">
+            Click here
+          </a>
         </div>
         <div className="project">
-        <h3>memory Game in Python</h3>
-        I will post a more detailed explanation about the game later<br />
-        to read the code: <a href="https://github.com/SimchaSucot/memoryGame" target="_blank">Click here</a>
+          <h3>memory Game in Python</h3>
+          I will post a more detailed explanation about the game later
+          <br /><br />
+          to read the code:
+          <a href="https://github.com/SimchaSucot/memoryGame" target="_blank">
+            Click here
+          </a>
         </div>
         <div className="project">
           Soon I will upload here the plans and projects with a detailed
@@ -24,7 +33,8 @@ function Projects() {
           explanation
         </div>
       </div>
-    </div>
+      </>
+    
   );
 }
 
