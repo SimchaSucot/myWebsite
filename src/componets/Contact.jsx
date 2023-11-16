09d9ea454e98d86ad17e8c274cedcdd0acc6ffce
Simcha Sucot <simchasucot@gmail.com>
2023-11-17 00:23:04 +0200

    added leetcode to the contact

--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -4,17 +4,18 @@ import imgLinkedin from "/linkedin-icon.png";
 import imgEmail from "/email_icon.png";
 import imgGithub from "/github_icon.png";
 import imgPhone from "/phone_icon.jpg";
+import imgLeetcode from "/leetcode.png";
 
 function Contact() {
   return (
     <>
       <div className="containerContact">
-        <h1>⇓ Contact me ⇓</h1>
+        <h1>⇓ Follow me or contact me ⇓</h1>
         <div className="links">
           <a href="https://api.whatsapp.com/send?phone=9720508726544&text=%D7%94%D7%99%D7%99" target="_blank">
             <img className="img" src={imgWhatsApp} alt="WhatsApp" />
           </a>
-          <a href="https://www.linkedin.com/in/simcha-sucot-935a9b263/" target="_blank">
+          <a href="https://www.linkedin.com/in/simcha-sucot-🇮🇱-935a9b263/" target="_blank">
             <img className="img" src={imgLinkedin} alt="Linkedin" />
           </a>
           <a href="mailto:simchasucot@gmail.com" target="_blank">
@@ -22,6 +23,9 @@ function Contact() {
           </a>
           <a href="https://github.com/SimchaSucot" target="_blank">
             <img className="img" src={imgGithub} alt="Github" />
+          </a>
+          <a href="https://leetcode.com/SimchaS/" target="_blank">
+            <img className="img" src={imgLeetcode} alt="leetCode" />
           </a>
           <a href="tel:+972508726544">
             <img className="img"src={imgPhone} alt="Phone" />
