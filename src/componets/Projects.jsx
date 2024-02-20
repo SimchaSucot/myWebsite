85ca63c45540161e548dbdb9e5cf458528c7bdb8
Simcha Sucot <simchasucot@gmail.com>
2024-02-20 16:28:15 +0200

    I added the plugin project and more...

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
       <div className="allProjects">
         <div className="project">
           <h3>This site is my project</h3>
-          Later I will upload a more detailed explanation here
+          This website showcases my qualifications, educational background, projects, profiles, and options to follow and contact me.
           <br />
           <br />
           to read the code:
@@ -27,6 +27,21 @@ function Projects() {
           <br />
           enter the page:
           <a href="https://simchasucot.github.io/Chat_socket/" target="_blank">
+            Click here
+          </a>
+        </div>
+        <div className="project">
+          <h3>Add-on for Google Chrome - Hebrew-English Text Swapper</h3>
+          A browser extension that automatically corrects text errors from typing in the wrong keyboard layout for Hebrew or English. It identifies the correct language and converts the characters accordingly. The corrected text can be easily copied back to the clipboard.
+          <br />
+          <br />
+          to read the code:
+          <a href="https://github.com/SimchaSucot/Keyboard_Fixer" target="_blank">
+            Click here
+          </a>
+          <br />
+          enter the page:
+          <a href="https://chromewebstore.google.com/detail/hlfeocfcjamaopiendpnolckamncpkai" target="_blank">
             Click here
           </a>
         </div>
