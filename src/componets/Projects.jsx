d6eef8c43b1ce6093e33de685aee0fd2e1b37e98
Simcha Sucot <simchasucot@gmail.com>
2024-06-30 22:36:42 +0300

    Added new Projets

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -16,6 +16,34 @@ function Projects() {
           </a>
         </div>
         <div className="project">
+          <h3>Contact Links</h3>
+          ContactLinks makes it super easy to create quick links for messaging and share your contact info. It's user-friendly and works great on any device. Whether you need to share your contact details professionally or just want an easy way to connect, ContactLinks has got you covered.
+          <br />
+          <br />
+          to read the code:
+          <a href="https://github.com/SimchaSucot/ContactLinks" target="_blank">
+            Click here
+          </a>
+          <br />
+          enter the page:
+          <a href="https://contact-links-six.vercel.app/" target="_blank">
+            Click here
+          </a>
+        </div>
+
+        <div className="project">
+          <h3>whatsapp bot</h3>
+          The WhatsApp Bot project uses Venom-bot and Express to handle weather and status queries via WhatsApp. It integrates with various APIs to provide real-time information and respond to user messages efficiently.
+          <br />
+          <br />
+          to read the code:
+          <a href="https://github.com/SimchaSucot/whatsapp-bot" target="_blank">
+            Click here
+          </a>
+          <br />
+        </div>
+
+        <div className="project">
           <h3>Chat (with socket)</h3>
           Later I will upload a more detailed explanation here
           <br />
